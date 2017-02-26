b89a3be1873a635d24dc20b312e6cba7d3c5c1c6
dakaii <daiki815@gmail.com>
2017-02-26 14:19:41 +0900

    a

--- client/client.js
+++ client/client.js
@@ -1,10 +1,23 @@
 import React from 'react'
 import { render } from 'react-dom'
 import App from '../components/App'
+import configureStore from '../redux/store'
+import { Provider } from 'react-redux'
+
+let initialState = {
+    todos: [{
+        id: 0,
+        completed: false,
+        text: 'Initial todo for demo purposes'
+    }]
+}
+
+let store = configureStore(initialState)
+
 
 render(
-    // define the emcompassing component,
-    // DOM element we want to mount it to
-    <App/>,
+    <Provider store={store}>
+        <App />
+    </Provider>,
     document.getElementById('app')    
 )
